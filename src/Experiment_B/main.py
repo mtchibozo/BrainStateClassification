e138b6d6ced73a3ed10a65006468a41577e63784
zwang233 <96964637+zwang233@users.noreply.github.com>
2022-01-04 16:25:04 -0500

    add plotting to main.py

--- src/Experiment_B/main.py
+++ src/Experiment_B/main.py
@@ -20,40 +20,55 @@ X_train,X_val,X_test,y_train,y_val,y_test = preprocessing.preprocess(folder_data
 
 history,path_prefix = train.training(folder_result,n_runs,X_train,X_val,X_test,y_train,y_val,y_test)
 
-import pandas as pd
-hist_df = pd.DataFrame(history.history)
-
-# save to json:
-hist_json_file = path_prefix + '_history.json'
-
-with open(hist_json_file, mode='w') as f:
-    hist_df.to_json(f)
-
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.show()
-
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('acc')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.show()
-
-# save to json:
-hist_json_file = os.path.join(path_prefix + '_history.json')
-
-with open(hist_json_file, mode='w') as f:
-    hist_df.to_json(f)
-
 # evaluating: load model for ROC resnet50_first_39runs_lr_adam_reduce_lr_rate0.3_reg0.0005_num_epochs1250_weights.best
 
 evaluate.roc_results(folder_result,n_runs,X_test,y_test)
+
+
+## plotting the best model
+
+model_type = 'resnet50_'
+
+reduce_lr_rate = 0.3
+learning_rate = 'adam_reduce_lr_rate'+str(reduce_lr_rate)#1e-5
+num_epochs = 1250
+
+for regularisation in [2e-4]:#, 9e-4, 1.25e-3, 2.5e-3, 5e-3]:
+    path_prefix = folder_result + model_type +'first_'+str(n_runs)+'runs_lr_'+str(learning_rate) + '_reg'+ str(regularisation) + '_num_epochs'+str(num_epochs)
+
+    checkpoint_path = path_prefix + '_weights.best.hdf5'
+    csv_logger_path = os.path.join(path_prefix,'.csv')
+
+import pandas as pd
+
+resnet_df = pd.read_csv(csv_logger_path,sep=";")
+resnet_df = resnet_df[resnet_df.epoch > 200]
+resnet_df['epoch'] = resnet_df['epoch'].apply(lambda x: x-200)
+
+plt.figure(figsize=(12,9))
+plt.plot(resnet_df["epoch"],resnet_df["loss"],color='navy',label='loss')
+plt.plot(resnet_df["epoch"],resnet_df["val_loss"],color='darkred',label='val_loss')
+plt.legend()
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.grid()
+plt.title('Loss and val loss as functions of epoch - 3D ResNet50 - (reg: '+str(regularisation)+')')
+#plt.yticks(np.arange(0.3,1,0.1))
+#plt.savefig(path_prefix + '_loss' +'.png')
+plt.show()
+
+plt.figure(figsize=(12,9))
+plt.plot(resnet_df["epoch"],resnet_df["val_acc"],color='darkred',label='val_accuracy')
+plt.plot(resnet_df["epoch"],resnet_df["acc"],color='navy',label='accuracy')
+plt.legend()
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.grid()
+plt.title('Accuracy and val accuracy as functions of epoch - 3D ResNet50 - (reg: ' + str(regularisation) + ')')
+#plt.savefig(path_prefix + '_acc' +'.png')
+plt.show()
+
+
 
 
 
